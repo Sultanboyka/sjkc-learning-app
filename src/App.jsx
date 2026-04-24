4296f3dc7c8a2748e5f0840629af0181f26a5f1a
Syaqeef Jamall <sj@Syaqeefs-MacBook-Pro.local>
2026-04-24 11:51:08 +0800

    fix build and ready for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 
 const subjects = [
   "Bahasa Melayu",
